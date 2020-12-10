ab4b710505a770039232bbc76516a000ed8d7c98
Eric Flumerfelt <eflumerf@fnal.gov>
2020-12-10 12:38:34 -0600

    Reorder usings and remove unused

--- include/dfmessages/Common.hpp
+++ include/dfmessages/Common.hpp
@@ -13,14 +13,14 @@
 
 namespace dunedaq {
 namespace dfmessages {
+using run_number_t = uint32_t;
 using trigger_number_t = uint64_t;
 using request_number_t = uint64_t;
+
 using timestamp_t = uint64_t;
 using timestamp_diff_t = int64_t;
-using type_t = unsigned;
-using run_number_t = uint32_t;
+
 using trigger_type_t = uint32_t;
-using error_flags_t = uint32_t;
 using system_time_t = uint64_t;
 
 } // namespace dfmessages
