9f1036498d06288f03b68c972de95c744c616a61
Eric Flumerfelt <eflumerf@fnal.gov>
2020-12-10 11:41:56 -0600

    Following guidance from the styleguide: https://github.com/DUNE-DAQ/styleguide/blob/develop/dune-daq-cppguide.md#45--structs-vs-pairs-and-tuples-dune-version

--- include/dfmessages/Common.hpp
+++ include/dfmessages/Common.hpp
@@ -12,11 +12,24 @@ using timestamp_t = uint64_t;
 using timestamp_diff_t = int64_t;
 using type_t = unsigned;
 using run_number_t = uint32_t;
-using roi_t = std::pair<timestamp_t, timestamp_t>;
+struct roi_t
+{
+  timestamp_t RegionStart;
+  timestamp_t RegionEnd;
+};
 using trigger_type_t = uint32_t;
 using error_flags_t = uint32_t;
-using geoID_t = std::pair<size_t, size_t>;
-using component_request_t = std::tuple<timestamp_t, timestamp_t, timestamp_diff_t>;
+struct geoID_t
+{
+  uint32_t APANumber;
+  uint32_t LinkNumber;
+};
+struct component_request_t
+{
+  timestamp_t RequestTimestamp;
+  timestamp_diff_t RequestOffset;
+  timestamp_t RequestWidth;
+};
 using component_request_map_t = std::map<geoID_t, component_request_t>;
 using system_time_t = uint64_t;
 
