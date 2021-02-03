5749fbad11cf149a0120d8470f58de673b68f13a
Philip Rodrigues <rodriges@fnal.gov>
2021-02-03 16:28:40 +0000

    Add plugins for each of the serializable classes

---
+++ plugins/TriggerDecisionNQ.cpp
@@ -0,0 +1,4 @@
+#include "networkqueue/NetworkQueueMacros.hpp"
+#include "dfmessages/TriggerDecision.hpp"
+
+DEFINE_DUNE_NETWORKQUEUE(dunedaq::dfmessages::TriggerDecision)
---
+++ plugins/TriggerInhibitNQ.cpp
@@ -0,0 +1,4 @@
+#include "networkqueue/NetworkQueueMacros.hpp"
+#include "dfmessages/TriggerInhibit.hpp"
+
+DEFINE_DUNE_NETWORKQUEUE(dunedaq::dfmessages::TriggerInhibit)
