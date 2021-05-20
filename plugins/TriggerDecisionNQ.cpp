416453c2f26608a3ac2aca11b0824964ecc8cf4f
Eric Flumerfelt <eflumerf@fnal.gov>
2021-05-20 14:52:45 -0500

    Run clang-format on files, implement some lint fixes.

--- plugins/TriggerDecisionNQ.cpp
+++ plugins/TriggerDecisionNQ.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file plugins/TriggerDecisionNQ.cpp TriggerDecision Network Queue Definition
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
 #include "dfmessages/TriggerDecision.hpp"
 #include "nwqueueadapters/AdapterMacros.hpp"
 
