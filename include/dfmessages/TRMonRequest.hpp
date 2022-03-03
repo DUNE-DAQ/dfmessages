a82f39f6ff3817faff42d939497976248e83e37c
Eric Flumerfelt <eflumerf@fnal.gov>
2022-03-03 14:44:30 -0600

    Format and apply linting changes.

--- include/dfmessages/TRMonRequest.hpp
+++ include/dfmessages/TRMonRequest.hpp
@@ -26,14 +26,10 @@ struct TRMonRequest
 
   request_number_t request_number{ TypeDefaults::s_invalid_request_number }; ///< The number of the request
   trigger_type_t trigger_type{ TypeDefaults::s_invalid_trigger_type }; ///< The trigger type that is being requested
-  run_number_t run_number{ TypeDefaults::s_invalid_run_number }; ///< The current run number
+  run_number_t run_number{ TypeDefaults::s_invalid_run_number };       ///< The current run number
   std::string data_destination; ///< The Monitoring destination that the TR should be sent to
 
-  DUNE_DAQ_SERIALIZE(TRMonRequest,
-                     request_number,
-                     trigger_type,
-                     run_number,
-                     data_destination);
+  DUNE_DAQ_SERIALIZE(TRMonRequest, request_number, trigger_type, run_number, data_destination);
 };
 
 /**
